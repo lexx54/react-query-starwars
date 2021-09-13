8c76b211b278223eb68a90835bd8a92f4b90ac4e
Lexington Chaparro <66759495+lexx54@users.noreply.github.com>
2021-09-13 18:44:03 -0400

    adds a basic pagination

--- src/components/Planets.jsx
+++ src/components/Planets.jsx
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "react-query";
 import axios from "axios";
 import Planet from "./Planet";
 
-const fetchPlanets = async () =>
-  await axios.get("https://swapi.dev/api/planets/");
+const fetchPlanets = async (page) =>
+  await axios.get(`https://swapi.dev/api/planets/?page=${page}`);
 
 const Planets = () => {
-  const { data, status } = useQuery("planet", fetchPlanets);
+  const [page, setPage] = useState(1);
+  const { data, status } = useQuery(["planet", page], ({ queryKey }) =>
+    fetchPlanets(queryKey[1])
+  );
   console.log(data?.data);
   console.log(status);
   return (
     <div>
       <h2>Planets</h2>
+      <button onClick={() => setPage(1)}> Page 1 </button>
+      <button onClick={() => setPage(2)}> Page 2 </button>
+      <button onClick={() => setPage(3)}> Page 3 </button>
       {status === "error" && <p>Error Fetching data </p>}
       {status === "loading" && <p>Loading data </p>}
       {status === "success" && (
